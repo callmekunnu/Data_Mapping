8cc19e331ea758905de4455e8f0fb8919b13a51e
Kunal A <kunal_aggarwal789@yahoo.com>
2022-01-15 18:09:15 +0530

    user class is added

---
+++ Data_Mapping/src/com/wipro/action/User.java
@@ -0,0 +1,29 @@
+package com.wipro.action;
+
+public class User {
+	
+	public String firstName;
+	public String lastName;
+	public String Address;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	
+	
+
+}
